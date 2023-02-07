3b905d852c0ad556981ddd149f1a0b42a2fe101f
KEIKO SHIMIZU <106935804+KeikoShimizu@users.noreply.github.com>
2023-02-06 23:21:21 -0800

    first commit ios converter complited code

--- Converter/ViewController.swift
+++ Converter/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tempText: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ConvertTemp(_ sender: Any) {
+        
+        guard let tempString = tempText.text
+        else {
+            return
+        }
+        
+        if let fahrenheit = Double (tempString){
+            let celsius = (fahrenheit - 32) / 1.8
+            let resultText = "Celsius \(celsius)"
+            resultLabel.text = resultText
+        }
+    }
+    
 }
+
+//23."guard statement" varify tempText field to have text.
+//23.tempText.text is accesing to the value inside text field
 
